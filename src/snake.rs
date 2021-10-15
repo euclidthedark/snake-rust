034cf8960159b441e70d9f188406a2ee30f46852
euclidthedark <euclidthedark@gmail.com>
2021-10-15 16:51:32 -0400

    create add_body_part to snake

--- src/snake.rs
+++ src/snake.rs
@@ -6,10 +6,17 @@ pub struct Snake {
 impl Snake {
     pub fn new(max_x: i32, max_y: i32) -> Snake {
 	// TODO:: Only allow even numbers for coordinates
-	Snake {
-		health: 3,
-		body_parts: vec!((max_x / 2, max_y / 2)),
-	}
+        Snake {
+            health: 3,
+            body_parts: vec!((max_x / 2, max_y / 2)),
+        }
+    }
+
+    pub fn add_body_part(&mut self) {
+        // TODO: perform a coordinate search to make sure body
+        // part addition does not collide with other body parts
+        let (x, y) = self.body_parts[self.body_parts.len() - 1];
+        self.body_parts.push((x + 1, y));
     }
 }
 
@@ -30,5 +37,13 @@ mod tests {
 
 	assert_eq!(snake.body_parts.len(), 1);
 	assert_eq!(snake.body_parts[0], (1, 1))
+    }
+
+    // TODO: add test cases to test for collisions
+    fn it_adds_a_body_part_to_the_snake() {
+        let mut snake = Snake::new(10, 10);
+
+        snake.add_body_part();
+        assert_eq!(snake.body_parts[snake.body_parts.len() - 1], (6, 5));
     }
 }

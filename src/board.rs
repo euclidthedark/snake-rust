0a34e19f3533124e50da2ea5ce08b21dec1fa4b4
euclidthedark <euclidthedark@gmail.com>
2021-10-15 20:11:42 -0400

    create set predator function

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,5 @@
+use rand::{thread_rng, Rng};
+
 pub struct Board {
     origin: (i32, i32),
     food: Option<(i32, i32)>,    
@@ -7,6 +9,7 @@ pub struct Board {
 }
 
 impl Board {
+
     pub fn new(max_x: i32, max_y: i32) -> Board {
         Board {
             max_x,
@@ -15,6 +18,16 @@ impl Board {
             food: None,
             predator: None,
         }
+    }
+
+    pub fn set_predator(&mut self) {
+        self.predator = Some(self.generate_board_position());
+    }
+
+    fn generate_board_position(&self) -> (i32, i32) {
+        let mut rng = thread_rng();
+
+        (rng.gen_range(0..self.max_x), rng.gen_range(0..self.max_y))
     }
 }
 
@@ -35,5 +48,14 @@ mod tests {
         let board = Board::new(10, 10);
 
         assert_eq!(board.origin, (5, 5));
+    }
+
+    // TODO: make test metamorphic
+    #[test]
+    fn it_creates_a_predator() {
+        let mut board = Board::new(10, 10);
+        board.set_predator();
+
+        assert_ne!(board.predator, None);
     }
 }
